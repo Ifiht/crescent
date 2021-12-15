e8a1a65bea2b58ec7a85f897c4fce34547275aef
Peter Pan <peter@never.lan>
2021-12-15 20:59:19 +0000

    start here

---
+++ eden/a.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+me = __FILE__
+child1 = rand(500..999)
+child2 = rand(100..499)
+
+system( "cat #{me} > #{child1}.rb" )
+system( "cat #{me} > #{child2}.rb" )
+
+if me.to_s.include?("a.rb")
+else
+    system( "rm #{me}" )
+end
