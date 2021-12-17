a9b9e7e23e8936999563784ae1ad01c134165dd3
Peter Pan <peter@never.lan>
2021-12-17 18:57:34 +0000

    update to include time stats

--- seed.rb
+++ seed.rb
@@ -15,8 +15,11 @@ h = { :x => 10, :y => 20, :z => 30 }
 
 
 child1 = rand(1..9)
+"eden2147483647/#{child1}.rb" == me.to_s ? child1 +=1 : nil
 child2 = rand(10..99)
+"eden2147483647/#{child2}.rb" == me.to_s ? child2 +=1 : nil
 child3 = rand(100..999)
+"eden2147483647/#{child3}.rb" == me.to_s ? child3 +=1 : nil
 
 
 system( "cat #{me} > ./eden2147483647/#{child1}.rb" )
