da40c71933d59e9b88904599453462db8ea5e48c
Peter Pan <peter@never.lan>
2021-12-17 18:11:13 +0000

    remove running dir edens

---
+++ seed.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+# Comment
+# Block
+# At
+# Beginning
+
+
+me = __FILE__
+
+
+a = [1, 2, 3, 4, 5]
+s = "And the earth was without form, and void; and darkness was upon the face of the deep"
+i = 2147483647
+h = { :x => 10, :y => 20, :z => 30 }
+
+
+child1 = rand(1..9)
+child2 = rand(10..99)
+child3 = rand(100..999)
+
+
+system( "cat #{me} > ./eden2147483647/#{child1}.rb" )
+system( "cat #{me} > ./eden2147483647/#{child2}.rb" )
+system( "cat #{me} > ./eden2147483647/#{child3}.rb" )
+
+
+if true
+    exit(251)
+elsif false
+    exit(13)
+else
+    exit(2)
+end
